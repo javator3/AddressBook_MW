88232741f157881e00019b80b254349cf1292867
motylla <magda.waliszewska@gmail.com>
2019-02-16 10:29:06 +0100

    Adding javafx

--- src/main/java/pl/sda/addressbook/Main.java
+++ src/main/java/pl/sda/addressbook/Main.java
@@ -1,8 +1,24 @@
 package pl.sda.addressbook;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
-	// write your code here
+        launch(args);
+
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/VerticalBox.fxml"));
+        primaryStage.setScene(new Scene(root, 600, 350));
+        primaryStage.show();
+
     }
 }
